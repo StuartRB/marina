8d947d9d68d79712de31c2f3d7ec717f42a36c96
Stuart Burnside <stuartburnside@Stuarts-Air.broadband>
2021-07-14 21:44:49 +0100

    Fixed it..

--- src/main/java/com/stuart/sandbox/model/Marina.java
+++ src/main/java/com/stuart/sandbox/model/Marina.java
@@ -4,27 +4,18 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Builder
+@Data
 public class Marina {
     private String name;
     private List<Boat> boats;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setBoats(List<Boat> boats) {
-        this.boats = boats;
-    }
-
     public List<Boat> getBoats() {
-        return boats == null ? new ArrayList<>() : boats;
+        if (boats == null) {
+            boats = new ArrayList<>();
+        }
+        return boats;
     }
 }
